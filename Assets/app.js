6888ebaee6330661f998e7b8af3ee2844be63c82
Caleb I <calebibejigba@gmail.com>
2023-01-04 21:14:57 +0000

    fixed the video

--- Assets/app.js
+++ Assets/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
   function movie() {
-     let apiurl = "https://api.themoviedb.org/3/search/movie?";
+    let apiurl = "https://api.themoviedb.org/3/search/movie?";
     let key = "api_key=408148785d4227b956248e0bb1647b96&query=";
-    let title = "creed "; 
-    let movievidoes = "&append_to_response=videos"
+    let title = "creed ";
+    let movievidoes = "&append_to_response=videos";
 
     $.ajax({
       url: apiurl + key + title + movievidoes,
@@ -11,18 +11,54 @@ $(document).ready(function () {
     }).then(function (response) {
       console.log(response);
 
-      
-  let moviecontainer = $("<img>")
-  let path = response.results[0].poster_path
-  let purl = "https://image.tmdb.org/t/p/w500/"
-  moviecontainer.attr('src', purl + path)
 
-  console.log(path)
+// for posters 
+      let moviecontainer = $("<img>");
+      let path = response.results[0].poster_path;
+      let purl = "https://image.tmdb.org/t/p/w500/";
+      moviecontainer.attr("src", purl + path);
 
-  $('.anything').append(moviecontainer)
+      console.log(path);
+
+     
+
+      // for movies 
+
+     
+
+   /*    let movievideo = $("<video>")
+      let source = "https://api.themoviedb.org/3/movie/312221/videos?api_key=408148785d4227b956248e0bb1647b96&language=en-US"
+      movievideo.attr("src", source) 
+ */
+
+      $(".anything").append(moviecontainer);
     });
+
+    $.ajax({
+
+        url:  "https://api.themoviedb.org/3/movie/312221/videos?api_key=408148785d4227b956248e0bb1647b96&language=en-US",
+        method: "GET",
+      }).then(function(response){
+
+        console.log(response)
+
+        
+        let youtube= "https://www.youtube.com/embed/"
+        let id = response.results[0].key
+        $('.videos').attr("src", youtube + id) 
+       
+
+
+
+console.log(youtube + id)
+        
+
+
+      }) 
   }
 
   movie();
-
 });
+
+
+
